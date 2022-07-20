f73ed09cabd52b210a801b5743bfdbde02c35efa
sarwar-jahin-port <109680869+sarwar-jahin-port@users.noreply.github.com>
2022-07-20 21:18:46 +0600

    session1 files uploaded

---
+++ session1/first_program.c
@@ -0,0 +1,12 @@
+// ctrl + shift + N -> keyboard shortcut(KS) to open new folder.
+// ctrl + s -> KS to save file
+// to check which compiler are you using settings > compiler(converts our code to computer understandable format.)
+
+#include <stdio.h>
+int main()
+{
+    printf("NAME: MOHAMMAD SARWAR JAHIN\nBATCH: CSE-54\nSECTION: D"); // here whatever we write into the double quotation("") will display as output. Ans (\n) means go to next line.
+
+    return 0;
+}
+// line 5-9 please remember it. We will understand its meaning(except line 8) end of this semester(sem). And at line 6 int we will understand it at 6th sem.
---
+++ session1/session1.c
@@ -0,0 +1,16 @@
+Some resources you need to follow along with the course
+1. Book - Author: Byron S. Gottfried
+        - Name: programming with C
+        - Edition: minimum 3rd edition.
+        - Note: 2nd edition is not acceptable because there is no seperate detail discussion about string.
+
+2. Other resources:
+    a. URI online judge
+    b. Codeforces - Newcomer sheet
+    c. atCoder/ CF/ vJudge contests
+    d. Course Outline
+
+
+// Note: If you can solve first 7 sheets from problem solving sheet(https://codeforces.com/group/MWSDmqGsZm/contests) you are good to go.
+// CodeBlocks is IBE = simple meaning is space to write.
+// Mobile users can use decoder.
